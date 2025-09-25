4cc64ead52bc6e1875d9dc6cefb26f9410e4b465
DESKTOP-BVRQTST\liaokuanya <liaokuanya@ldcems.com>
2025-09-25 20:07:40 +0800

    feat: 构建配置

--- frontend/src/components/home/sections/navbar.tsx
+++ frontend/src/components/home/sections/navbar.tsx
@@ -230,7 +230,7 @@ export function Navbar() {
                       priority
                     />
                     <span className="font-medium text-primary text-sm">
-                      / Suna
+                      / AniSora
                     </span>
                   </Link>
                   <button
