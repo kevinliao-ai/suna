d14ef7a345e3581e7226c7c4bcb9739ae4e17af2
DESKTOP-BVRQTST\liaokuanya <liaokuanya@ldcems.com>
2025-11-17 18:33:28 +0800

    feat: 更新url

--- frontend/src/app/(dashboard)/dashboard/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/page.tsx
@@ -250,7 +250,7 @@ export default function DashboardPage() {
                       <div className="flex items-center justify-center gap-2">
                         <a
                           className="underline"
-                          href="https://indexteam-indextts-2-demo.ms.show/?t=1763375240052&__theme=light&studio_token=a63647de-f801-4322-8844-074591ae45e8&backend_url=/"
+                          href="https://indexteam-indextts-2-demo.ms.show/?__theme=light&studio_token=a63647de-f801-4322-8844-074591ae45e8&backend_url=/"
                           target="_blank"
                           rel="noreferrer"
                         >Open in new window</a>
@@ -272,7 +272,7 @@ export default function DashboardPage() {
                     <iframe
                       id="index-tts-iframe"
                       title="Index-TTS Demo"
-                      src="https://indexteam-indextts-2-demo.ms.show/?t=1763375240052&__theme=light&studio_token=a63647de-f801-4322-8844-074591ae45e8&backend_url=/"
+                      src="https://indexteam-indextts-2-demo.ms.show/?__theme=light&studio_token=a63647de-f801-4322-8844-074591ae45e8&backend_url=/"
                       className="h-full w-full border-0"
                       sandbox="allow-scripts allow-forms allow-same-origin allow-popups allow-modals"
                       allow="microphone; autoplay"
